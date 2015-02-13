56597af4909cadff846691e602a1e319e5056c79
Roman <RomzesRover@gmail.com>
2015-02-13 16:51:44 +0500

    AUS *11 start service automatically with boot up

---
+++ src/com/BBsRs/vkaudiosync/Services/BootUpCatcherReceiver.java
@@ -0,0 +1,30 @@
+package com.BBsRs.vkaudiosync.Services;
+
+import org.holoeverywhere.preference.PreferenceManager;
+import org.holoeverywhere.preference.SharedPreferences;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.BBsRs.vkaudiosync.VKApiThings.Constants;
+
+public class BootUpCatcherReceiver extends BroadcastReceiver {
+	
+	//preferences 
+    SharedPreferences sPref;
+    
+    String LOG_TAG = "BootReceiver";
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		//set up preferences
+        sPref = PreferenceManager.getDefaultSharedPreferences(context);
+        if (sPref.getBoolean(Constants.PREFERENCE_AUTOMATIC_SYNCHRONIZATION, false)){
+        	Log.i(LOG_TAG, "start service");
+        	context.startService(new Intent(context, AutomaticSynchronizationService.class));
+        }	
+	}
+
+}

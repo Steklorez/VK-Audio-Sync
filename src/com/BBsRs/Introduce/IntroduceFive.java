fd3eb90b1a4b0a9bb6ce4d1a39f19002b1d4cf13
Roman <RomzesRover@gmail.com>
2015-02-22 16:19:55 +0500

    add join group on intro

--- src/com/BBsRs/Introduce/IntroduceFive.java
+++ src/com/BBsRs/Introduce/IntroduceFive.java
@@ -8,27 +8,38 @@ import org.holoeverywhere.widget.AdapterView.OnItemSelectedListener;
 import org.holoeverywhere.widget.Button;
 import org.holoeverywhere.widget.CheckBox;
 import org.holoeverywhere.widget.Spinner;
+import org.holoeverywhere.widget.Toast;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 
 import com.BBsRs.vkaudiosync.LoaderActivity;
 import com.BBsRs.vkaudiosync.R;
+import com.BBsRs.vkaudiosync.VKApiThings.Account;
 import com.BBsRs.vkaudiosync.VKApiThings.Constants;
+import com.perm.kate.api.Api;
 
 public class IntroduceFive extends Activity {
 
 	//preferences 
     SharedPreferences sPref;
     
-    CheckBox aus, ausWifi;
+    CheckBox aus, ausWifi, joinGroup;
     Spinner frequency;
     
     int check=0;
+    
+    private final Handler handler = new Handler();
+    
+    /*----------------------------VK API-----------------------------*/
+    Account account=new Account();
+    Api api;
+    /*----------------------------VK API-----------------------------*/
 
 	/** Called when the activity is first created. */
 	@Override
@@ -43,13 +54,51 @@ public class IntroduceFive extends Activity {
 	    
 	    //set up preferences
         sPref = PreferenceManager.getDefaultSharedPreferences(this);
+        
+        /*----------------------------VK API-----------------------------*/
+    	//retrieve old session
+        account.restore(getApplicationContext());
+        
+        //create new session
+        if(account.access_token!=null)
+            api=new Api(account.access_token, Constants.API_ID);
+        /*----------------------------VK API-----------------------------*/
 	    
 	    Button next = (Button)this.findViewById(R.id.next);
 	    Button back = (Button)this.findViewById(R.id.back);
+	    joinGroup = (CheckBox)this.findViewById(R.id.joinGroup);
 	    
 	    next.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
+				//join group
+				if (joinGroup.isChecked()){
+					sPref.edit().putBoolean(Constants.CLICKED_SUBSCRIBE, true).commit();
+					
+					new Thread (new Runnable(){
+						@Override
+						public void run() {
+							try {
+								api.joinGroup(Constants.GROUP_ID, null, null);
+								handler.post(new Runnable (){
+									@Override
+									public void run() {
+										Toast.makeText(getApplicationContext(), getString(R.string.join_group_success), Toast.LENGTH_LONG).show();
+									}
+								});
+							} catch (Exception e) {
+								handler.post(new Runnable (){
+									@Override
+									public void run() {
+										Toast.makeText(getApplicationContext(), getString(R.string.error_message), Toast.LENGTH_LONG).show();
+									}
+								});
+								e.printStackTrace();
+							}
+						}
+					}).start();
+				} 
+				
 				startActivity(new Intent(getApplicationContext(), LoaderActivity.class));
 				overridePendingTransition(R.anim.push_right_in, R.anim.push_right_out);
 				finish();
